a387f5acf03e5913f824fb9aa9250d1e51d11dfb
Hayden Nesbit <hayden.nesbit@campusoutreach.org>
2020-04-27 19:56:37 -0400

    added reactstrap navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,88 +1,92 @@
-import React from "react"
+// import React from "react"
 
-class Navbar extends React.Component {
-    constructor(props) {
-        super(props)
-        this.navbarLinks = [
-            "About",
-            "Blog",
-            "Projects"
-        ]
-    }
+// class Navbar extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.navbarLinks = [
+//             "About",
+//             "Blog",
+//             "Projects"
+//         ]
+//     }
 
-    render() {
-        const navLinksinHTML = this.navbarLinks.map((link, index) => {
-            return (
-                <React.Fragment key={index}>
-                    <a className="text-white pl-5"
-                        onClick={() => this.props.updatePage(link)}
-                        href="#"
+//     render() {
+//         const navLinksinHTML = this.navbarLinks.map((link, index) => {
+//             return (
+//                 <React.Fragment key={index}>
+//                     <a className="text-white pl-5"
+//                         onClick={() => this.props.updatePage(link)}
+//                         href="#"
 
-                    >
-                        {link}
-                    </a>
-                </React.Fragment>
-            )
-        })
-        return (
-            <div className="bg-dark p-3">
-                <div className="d-inline-flex">
-                <h3 className="text-white">Hayden Nesbit</h3>
-                   <h6 className="pt-2">{navLinksinHTML}</h6> 
-                </div>
-            </div>
-        )
-    }
-}
-
-export default Navbar
-
-
-
-
-// import React, { useState } from 'react';
-// import { Navbar, Nav, NavItem, NavLink, NavbarToggler, Collapse } from 'reactstrap';
-
-// const TopNav = (props) => {
-
-//     const [isOpen, setIsOpen] = useState(false);
-
-//     const toggle = () => setIsOpen(!isOpen);
-
-
-//     return (
-//         <div className="bg-dark p-3">
-//             <Navbar light expand="md">
-//                 <NavbarToggler onClick={toggle} />
-//                 <Collapse isOpen={isOpen} navbar>
-//                     <Nav>
-//                         <NavItem>
-//                             <NavLink className="text-white"><h4>Hayden Nesbit</h4></NavLink>
-//                         </NavItem>
-//                     </Nav>
-//                     <Nav>
-//                         <NavItem>
-//                             <NavLink href=""><h5>About</h5></NavLink>
-//                         </NavItem>
-//                     </Nav>
-//                     <Nav>
-//                         <NavItem>
-//                             <NavLink href=""><h5>Blog</h5></NavLink>
-//                         </NavItem>
-//                     </Nav>
-//                     <Nav>
-//                         <NavItem>
-//                             <NavLink href=""><h5>Projects</h></NavLink>
-//                         </NavItem>
-//                     </Nav>
-//                 </Collapse>
-//             </Navbar>
-//         </div>
-
-//     )
+//                     >
+//                         {link}
+//                     </a>
+//                 </React.Fragment>
+//             )
+//         })
+//         return (
+//             <div className="bg-dark p-3">
+//                 <div className="d-inline-flex">
+//                 <h3 className="text-white">Hayden Nesbit</h3>
+//                    <h6 className="pt-2">{navLinksinHTML}</h6> 
+//                 </div>
+//             </div>
+//         )
+//     }
 // }
 
-// export default TopNav;
+// export default Navbar
+
+
+
+
+import React, { useState } from 'react';
+import { Navbar, Nav, NavItem, NavLink, NavbarToggler, Collapse } from 'reactstrap';
+
+function TopNav(props) {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
+    let navbarLinks = [
+        "About",
+        "Blog",
+        "Projects"
+    ]
+    const navLinksinHTML = navbarLinks.map((link, index) => {
+        return (
+            <Nav>
+                <NavItem>
+                    <NavLink key={index} className="text-white pl-3"
+                        onClick={() => props.updatePage(link)}
+                        href="#"
+                    >
+                        <h6>{link}</h6>
+                    </NavLink>
+                </NavItem>
+            </Nav>
+         ) })
+
+
+    return (
+            <div className="bg-dark p-3">
+                <Navbar light expand="md">
+                    <NavbarToggler onClick={toggle} />
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav>
+                            <NavItem>
+                                <NavLink className="text-white"><h4>Hayden Nesbit</h4></NavLink>
+                            </NavItem>
+                        </Nav>
+                        {navLinksinHTML}
+                    </Collapse>
+                </Navbar>
+            </div>
+
+        )
+    }
+
+export default TopNav;
 
 
 
