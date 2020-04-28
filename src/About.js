2911fea0499791d06331842fd7e4c66c02e4214a
Hayden Nesbit <hayden.nesbit@campusoutreach.org>
2020-04-28 07:35:08 -0400

    full responsiveness

--- src/About.js
+++ src/About.js
@@ -8,9 +8,9 @@ class About extends React.Component {
         return (
             <div id="famImg" className="container">
                 <div className="row">
-                    <div className="col-md-4 offset-4 mt-5 text-center">
+                    <div className="col mt-5 text-center">
                         <img src={img} />
-                        <p className="mt-4">A blog-in-progress documenting my growth as a Full-Stack Developer.</p>
+                        <p className="mt-4">A blog-in-progress documenting my growth <br /> as a Full-Stack Developer.</p>
                         <div className="d-inline">
                             <a className="pr-4" href="https://github.com/hayden-nesbit?tab=repositories" target="_blank"><FontAwesomeIcon icon={faGithub} size="2x" /></a>
                             <a className="pr-4" href="https://www.linkedin.com/in/hayden-nesbit-b54a7485/" target="_blank"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
